d667b0926a5afedb1f3757152c3c253ebd42eafb
Kartik <kartik15nov@gmail.com>
2021-01-10 19:12:32 +0530

    Initial Commit.

---
+++ src/main/java/com/ub/inventoryfailover/web/RouteConfig.java
@@ -0,0 +1,22 @@
+package com.ub.inventoryfailover.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
+public class RouteConfig {
+
+    @Bean
+    public RouterFunction<ServerResponse> inventoryRoute(InventoryHandler inventoryHandler) {
+        return route(GET("/inventory-failover")
+                        .and(accept(MediaType.APPLICATION_NDJSON)),
+                inventoryHandler::listInventory);
+    }
+}
